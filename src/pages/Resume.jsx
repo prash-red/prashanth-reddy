8e27fbd8d5e2ec7d50604dd711bbb8dbed25b94e
prashanth <shyamalaprashanth2004@gmail.com>
2026-08-05 02:48:41 +0400

    Add Resume page content

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,18 @@
 import PageTransition from '../layout/PageTransition.jsx'
+import { asset } from '../utils/asset.js'
+import './Resume.css'
 
 export default function Resume() {
     return (
         <PageTransition>
-            <div className="page-wrap">
+            <div className="page-wrap" style={{ textAlign: 'center' }}>
                 <h1 className="pixel-text">Resume</h1>
+                <iframe title="Resume" src={asset('assets/docs/resume.pdf')} className="pdf">
+                    <p>
+                        Unable to display PDF file.{' '}
+                        <a href={asset('assets/docs/resume.pdf')}>Download</a> instead.
+                    </p>
+                </iframe>
             </div>
         </PageTransition>
     )
